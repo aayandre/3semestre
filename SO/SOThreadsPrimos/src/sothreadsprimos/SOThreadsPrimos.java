383c4ce1d25e127e8ab8cb985492177500e1a8a8
André <aayandre94@gmail.com>
2018-05-27 22:58:53 -0300

    Inicio

---
+++ SO/SOThreadsPrimos/src/sothreadsprimos/SOThreadsPrimos.java
@@ -0,0 +1,21 @@
+/*
+ * André de Amorim Yamamoto
+ * TADS - Turma A
+ * aay.andre@outlook.com
+ */
+package sothreadsprimos;
+
+/**
+ *
+ * @author aayan
+ */
+public class SOThreadsPrimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
